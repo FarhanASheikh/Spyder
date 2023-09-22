ef628b8d850dc3e84d4ef683cc65f3a16e29e44d
FarhanAhmed <farhan.sheikh@daraz.pk>
2023-09-22 16:48:47 +0500

    Java Update

--- src/test/java/Utilities/Wait.java
+++ src/test/java/Utilities/Wait.java
@@ -3,11 +3,15 @@ package Utilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
+
 public class Wait{
     public static WebDriver driver;
     public static WebDriver wait =null;
     public static WebDriverWait waitforelement() {
-         return new WebDriverWait(driver, 60);
+        int seconds = 60; // Replace this with your desired number of seconds
+        Duration duration = Duration.ofSeconds(seconds);
+         return new WebDriverWait(driver, duration);
     }
 
 
